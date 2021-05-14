46089c5cd617428c664b7b0d38d1f15120d0b996
Annie Liao <liaowow@gmail.com>
2021-05-14 17:21:11 -0400

    complete exercise 5 by coding along

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -29,6 +29,7 @@ const Header = () => {
           <NavLink href="/new">New&nbsp;Releases</NavLink>
           <NavLink href="/men">Men</NavLink>
           <NavLink href="/women">Women</NavLink>
+          <NavLink href="/women">Women</NavLink>
           <NavLink href="/kids">Kids</NavLink>
           <NavLink href="/collections">Collections</NavLink>
         </DesktopNav>
@@ -61,8 +62,8 @@ const MainHeader = styled.div`
   display: flex;
   align-items: baseline;
   padding: 18px 32px;
-  height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  overflow: auto;
 
   @media ${QUERIES.tabletAndSmaller} {
     border-top: 4px solid ${COLORS.gray[900]};
@@ -77,7 +78,11 @@ const MainHeader = styled.div`
 
 const DesktopNav = styled.nav`
   display: flex;
-  gap: 48px;
+  gap: clamp(
+    1rem,
+    8.1vw - 4rem,
+    3rem
+  );
   margin: 0px 48px;
 
   @media ${QUERIES.tabletAndSmaller} {
